2ebb608122e1f3d04aa0ecf79e907b8b089cac84
Trezor <trezoro@mitylite.com>
2017-10-27 07:05:46 -0600

    working through examples and found a better way to do form input, updated cluncky.

--- app/assets/Clunky_Database/app/assets/scripts/app.js
+++ app/assets/Clunky_Database/app/assets/scripts/app.js
@@ -1,7 +1,3 @@
-import DrawLineGraph from './modules/DrawLineGraph';
-import TabThroughForm from './modules/TabThroughForm';
-import TestInputs from './modules/TestInputs.js';
+import MongoDB from './modules/MongoDB';
 
-var testInputs = new TestInputs();
-var drawDemoGraph = new DrawLineGraph();
-var tabThroughForm = new TabThroughForm();
+var myMongoDB = new MongoDB();
